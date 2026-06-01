4a8b99bcf92cc537800de545583c86123abc82db
ByronMckitrick <mckitricklbyron@gmail.com>
2026-06-01 09:51:38 -0600

    scratch weather

---
+++ scratch/weather/weather-api.js
@@ -0,0 +1,10 @@
+export function getWeatherData(){
+    const data = {
+        "location": "Rexburg",
+        "conditions": "Sunny",
+        "icon": "&#9728;",
+        "temp": 50,
+        "windspeed": 2 
+    }
+    return data
+}
